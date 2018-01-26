0744609186260bb00d2ed14d0b977cbc262449e3
17leba <17leba@gmail.com>
2018-01-26 13:57:44 +0800

    添加发布代码

--- publish.sh
+++ publish.sh
@@ -1,4 +1,14 @@
-npm run build
-cp -rf dist/vue ../static/
-cp -rf dist/index.html ../views/
-rm -rf dist
+COMMIT_ID=""$1
+
+echo '****************更新代码***************'
+git reset --hard || { echo "command failed"; exit 1; }
+git fetch -p origin || { echo "command failed"; exit 1; }
+git checkout $COMMIT_ID || { echo "command failed"; exit 1; }
+
+echo '****************编译代码***************'
+npm run build || { echo "command failed"; exit 1; }
+
+echo '****************发布代码***************'
+cp -rf dist/vue ./../koa-api/static/ || { echo "command failed"; exit 1; }
+cp -rf dist/index.html ./../koa-api/views/ || { echo "command failed"; exit 1; }
+rm -rf dist || { echo "command failed"; exit 1; }
